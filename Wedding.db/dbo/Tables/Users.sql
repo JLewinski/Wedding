10aae1a467182e7ad12d39f9567dbac18b47377a
Jacob Lewinski <jdlewinski@outlook.com>
2023-05-23 14:21:51 -0500

    Forgot about this

--- Wedding.db/dbo/Tables/Users.sql
+++ Wedding.db/dbo/Tables/Users.sql
@@ -3,6 +3,7 @@
     [UserName]       NVARCHAR (128)   NOT NULL,
     [HashedPassword] NVARCHAR (MAX)   NOT NULL,
     [IsAdmin]        BIT              CONSTRAINT [DF_Users_IsAdmin] DEFAULT ((0)) NOT NULL,
+    [DateCreated]    DATETIME         NOT NULL DEFAULT(GETDATE()),
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
 );
 
