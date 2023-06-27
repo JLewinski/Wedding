f02265fe5607f8e0f4bb5836efcd81d954555fcf
Jacob Lewinski <jdlewinski@outlook.com>
2023-06-27 10:20:59 -0500

    Added info to readme and a comment in the sql

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Wedding
+
+I used [this git repo](https://github.com/rampatra/wedding-website) as a template. Then I completely redesigned it to use dotnet core and typescript.
+
+This site is hosted on a shared server provided by GoDaddy
--- Wedding.db/dbo/Tables/Guests.sql
+++ Wedding.db/dbo/Tables/Guests.sql
@@ -1,4 +1,8 @@
-﻿CREATE TABLE [dbo].[Guests] (
+﻿--Was going to allow every guest to sign in to see and change their RSVP, but decided against it.
+--I didn't want the users to have to worry about creating a username / password
+--Because of this, there's not really a need for the User table to be connected to the Guest table
+--I just didn't want to change other things that were already working, so I left it as is.
+CREATE TABLE [dbo].[Guests] (
     [UserId]         UNIQUEIDENTIFIER NOT NULL,
     [NumberAdults]   TINYINT          NOT NULL,
     [NumberChildren] TINYINT          NOT NULL,
@@ -9,5 +13,6 @@
     [GuestId]        INT              IDENTITY (1, 1) NOT NULL,
     [DateModified]   DATETIME         NOT NULL DEFAULT(GETDATE()),
     CONSTRAINT [IX_Guests] UNIQUE NONCLUSTERED ([UserId] ASC)
+
 );
 
